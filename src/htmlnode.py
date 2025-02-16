4ecd5f4824fc0a58da44db98e236fe9e77d52ccc
aaron <aaron@noreply.com>
2025-02-16 22:43:30 +0800

    Add LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -22,7 +22,25 @@ class HTMLNode:
         if self.props is None:
             return ""
 
-        return " ".join((f'{k}="{v}"' for k, v in self.props.items()))
+        return " " + " ".join(f'{k}="{v}"' for k, v in self.props.items())
 
     def __repr__(self) -> str:
         return f"tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props}"
+
+
+class LeafNode(HTMLNode):
+
+    def __init__(
+        self, tag: Optional[str], value: str, props: dict[str, str] | None = None
+    ) -> None:
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self) -> str:
+
+        if self.value is None:
+            raise ValueError("All leaf nodes must have a value.")
+
+        if self.tag is None:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
